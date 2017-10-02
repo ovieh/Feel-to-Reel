5b9cc2ef13cb31a3353247d6f07722115ab95271
tawnycat <tawnyeleanor@gmail.com>
2017-10-02 19:39:42 -0400

    set up first if statement

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,12 @@ $(document).on("click", "SUBMIT BUTTON", displayMovies);
 
 function displayMovies () {
 
-// AJAX call to the Giphy API
+// AJAX call to the Movie Database API
 
 $("#movie-view").empty();
 
+
+//// Which genre will match with which emotion? 
 // anger = action, crime, thriller
 // contempt = documentary, history
 // disgust = science fiction
@@ -15,26 +17,54 @@ $("#movie-view").empty();
 // surprise = fantasy, adventure
 // happiness = comedy, music
 
-var action = "";
-var adventure = "";
-var comedy = "";
-var crime = "";
-var documentary = "";
-var drama = "";
-var fantasy = "";
-var history = "";
-var horror = "";
-var music = "";
-var mystery = "";
-var romance = "";
-var scienceFiction = "";
-var thriller = "";
+// Variables holding the genre IDs needed for the API call
 
-var queryURL = 
+var action = "28";
+var adventure = "12";
+var comedy = "35";
+var crime = "80";
+var documentary = "99";
+var drama = "18";
+var fantasy = "14";
+var history = "36";
+var horror = "27";
+var music = "10402";
+var mystery = "9648";
+var romance = "10749";
+var scienceFiction = "878";
+var thriller = "53";
 
-$.ajax({
-	url: queryURL,
-	method: 'GET'
-}).done(function(response) {
+// Empty variable to hold URL which will change depending on emotion detected
 
-	console.log(response);
+var queryURL = "";
+
+// Movie Database API key
+
+var = movieAPI = "aed8a1ce3108479482ae5d0e4cbb536a";
+
+// If statements that determine which URL will be called
+
+if (emotion === anger) {
+
+	queryURL = "https://api.themoviedb.org/3/discover/movie?api_key=" + movieAPI + "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=" + action + "%2C%20" + crime + "%2C%20" + thriller; 
+
+} else if (emotion === contempt) {
+
+	
+
+}
+}
+
+// AJAX Call to the Movie Database API
+
+function ajaxCall () {
+
+	$.ajax({
+		url: queryURL,
+		method: 'GET'
+	}).done(function(response) {
+
+		console.log(response);
+
+	}
+}
