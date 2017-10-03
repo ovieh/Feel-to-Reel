dd8770bc6345df66f43f2f3326d9dd9bdb2c7b09
Ovieh <ovieh.mosley@gmail.com>
2017-10-03 00:55:56 -0400

    returns emotion api json.

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,12 +24,39 @@ button.onclick = function () {
   canvas.getContext('2d').
   drawImage(video, 0, 0, canvas.width, canvas.height);
 
-  dataURL = canvas.toDataURL();
+  dataURL = canvas.toDataURL("image/png");
 
+  var makeblob = function (dataURL) {
+    var BASE64_MARKER = ';base64,';
+    if (dataURL.indexOf(BASE64_MARKER) == -1) {
+      var parts = dataURL.split(',');
+      var contentType = parts[0].split(':')[1];
+      var raw = decodeURIComponent(parts[1]);
+      return new Blob([raw], {
+        type: contentType
+      });
+    }
+    var parts = dataURL.split(BASE64_MARKER);
+    var contentType = parts[0].split(':')[1];
+    var raw = window.atob(parts[1]);
+    var rawLength = raw.length;
+
+    var uInt8Array = new Uint8Array(rawLength);
+
+    for (var i = 0; i < rawLength; ++i) {
+      uInt8Array[i] = raw.charCodeAt(i);
+    }
+
+    return new Blob([uInt8Array], {
+      type: contentType
+    });
+  };
+
+  // console.log(dataURL);
   var params = {
     // Request parameters
   };
-  
+
   $.ajax({
       // NOTE: You must use the same location in your REST call as you used to obtain your subscription keys.
       //   For example, if you obtained your subscription keys from westcentralus, replace "westus" in the 
@@ -37,15 +64,17 @@ button.onclick = function () {
       url: "https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize?" + $.param(params),
       beforeSend: function (xhrObj) {
         // Request headers
-        xhrObj.setRequestHeader("Content-Type", "application/json");
-  
+        xhrObj.setRequestHeader("Content-Type", "application/octet-stream");
+
         // NOTE: Replace the "Ocp-Apim-Subscription-Key" value with a valid subscription key.
         xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "d8c7aa1767df41c0aa08d36223895b0c");
       },
       type: "POST",
       // Request body
-      // data: '{"url": "http://vandammethatface.com/images/face.png"}',
-      data: [dataURL]
+      // data: '{"url": canvas.toDataURL()}',
+      data: makeblob(dataURL),
+      processData: false,
+      
     })
     .done(function (data) {
       alert("success");
@@ -54,7 +83,7 @@ button.onclick = function () {
     .fail(function () {
       alert("error");
     });
-  
+
 };
 console.log(dataURL);
 
@@ -74,5 +103,3 @@ function handleError(error) {
 
 navigator.mediaDevices.getUserMedia(constraints).
 then(handleSuccess).catch(handleError);
-
-
