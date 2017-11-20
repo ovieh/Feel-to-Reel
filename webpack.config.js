40305e48459f5bd7c5501adb6b95076cd97c0a48
Ovieh <ovieh.mosley@gmail.com>
2017-11-19 22:15:31 -0500

    still working on webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,20 @@
 const path = require('path');
 // webpack.config.js 
 const Dotenv = require('dotenv-webpack');
+const ASSET_PATH = process.env.ASSET_PATH || '/';
+
 
 module.exports = {
   entry: './public/assets/js/script.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: ASSET_PATH
   },
   plugins: [
     new Dotenv({
       path: './.env', // Path to .env file (this is the default) 
-      safe: false // load .env.example (defaults to "false" which does not use dotenv-safe) 
+      safe: true // load .env.example (defaults to "false" which does not use dotenv-safe) 
     })
   ]
 };
